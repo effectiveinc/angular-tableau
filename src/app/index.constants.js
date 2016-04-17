e6ab18098794eaa032f8f4e33030bdfd8c64d41c
Ryan Bell <ryan.bell@effectiveui.com>
2016-04-16 21:50:05 -0400

    Scaffold project with generator-gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTableau')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
